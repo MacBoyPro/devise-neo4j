42f7c101d12f22de15a459b7401ad43e528744b3
MacBoyPro <lee.lundrigan@gmail.com>
2014-03-23 01:17:47 -0700

    test for compatibility

--- lib/devise/orm/neo4j/compatibility.rb
+++ lib/devise/orm/neo4j/compatibility.rb
@@ -11,10 +11,16 @@ module Devise
          # def assign_attributes(new_attributes)
          #   attributes = sanitize_for_mass_assignment(new_attributes)
          # end
-        
+
+          def self.serialize_from_session(key, salt)
+            record = self.find(key.first)
+            record if record && record.authenticatable_salt == salt
+          end     
+
+          def self.find_first_by_auth_conditions(tainted_conditions, opts={})
+            self.find(devise_parameter_filter.filter(tainted_conditions).merge(opts))
+          end
         end
-        
-      
       end
     end
   end
